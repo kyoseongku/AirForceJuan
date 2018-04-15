860ba6bc90b3c5ee2393769822ad0b7173ac4ab7
Kyoseong Ku <kyoseongku@gmail.com>
2018-04-14 19:28:26 -0700

    Backup

--- AirForceJuan.go
+++ AirForceJuan.go
@@ -21,31 +21,40 @@ func middlewares(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 
 
 
-func phoneHandler(http.ResponseWriter, *http.Request) {
+func phoneHandler(res http.ResponseWriter, req *http.Request) {
   where := "phone"
   log.Println(where, "hello")
+
+  data := []byte("phone handler")
+  res.Write(data)
 }
 
 
 
-func rpiHandler(http.ResponseWriter, *http.Request) {
+func rpiHandler(res http.ResponseWriter, req *http.Request) {
   where := "rpi"
   log.Println(where, "hello")
+
+  data := []byte("rpi handler")
+  res.Write(data)
 }
 
 
 
-func webHandler(http.ResponseWriter, *http.Request) {
+func webHandler(res http.ResponseWriter, req *http.Request) {
   where := "web"
   log.Println(where, "hello")
+
+  data := []byte("web handler")
+  res.Write(data)
 }
 
 
 
 func main() {
-  http.HandleFunc("phone", middlewares(phoneHandler))
-  http.HandleFunc("rpi", middlewares(rpiHandler))
-  http.HandleFunc("web", middlewares(webHandler))
+  http.HandleFunc("/phone", middlewares(phoneHandler))
+  http.HandleFunc("/rpi", middlewares(rpiHandler))
+  http.HandleFunc("/web", middlewares(webHandler))
 
   log.Println("Launching server", port)
   err := http.ListenAndServe(port, nil)
